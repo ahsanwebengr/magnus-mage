fa4cd690ab55aa62fdd3d928d23c7bb2b43e66c2
ahsanwebengr <ahsanwebengr@gmail.com>
2023-12-25 20:29:18 +0500

    fix: password validation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -28,7 +28,11 @@ const Register = () => {
       .required('Email is required'),
     password: yup
       .string()
-      .required("Password is required"),
+      .required("Password is required")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character'
+      ),
     confirmPassword: yup
       .string()
       .oneOf([yup.ref('password'), null], 'Passwords must match')
